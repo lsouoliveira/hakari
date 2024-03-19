7f010164f6e13bd88f4837edebeffd9a5d56b010
Lucas Sousa <ls442336@gmail.com>
2024-03-18 23:11:08 -0300

    Add levels support to logging

--- lib/hakari/logging.rb
+++ lib/hakari/logging.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
-def log(message = "")
-  puts message
+def log(message = "", level = :info)
+  output = ""
+  output += Time.now.strftime("%H:%M:%S")
+  output += " "
+  output += "[#{level.to_s.upcase}]".colorize(level_color(level))
+  output += " "
+  output += message
+
+  puts output
+end
+
+def level_color(level)
+  case level
+  when :info
+    :green
+  when :error
+    :red
+  else
+    :white
+  end
 end
